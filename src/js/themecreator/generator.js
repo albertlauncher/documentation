5030a449e10ee4c6c8182ada86598b4f4ad91105
Fuhrmann <fuhrmanns@gmail.com>
2017-01-28 22:08:01 -0300

    Change the way the default theme is loaded, so we can open the possibility to load another theme as the default one

--- src/js/themecreator/generator.js
+++ src/js/themecreator/generator.js
@@ -1,3 +1,5 @@
+var themes, defaultTheme = "Bright";
+
 main();
 
 /**
@@ -5,41 +7,22 @@ main();
  */
 function main() {
     initClipboard();
+    loadThemes(function () {
+        // And finally display albert to the public
+        document.getElementById('albert').style.display = 'block';
+        document.getElementById('options').style.display = 'block';
+    });
 
     // Get all inputs with the 'style' class and add an event listener
     var inputs = [].slice.call(document.querySelectorAll('.style'));
     inputs.forEach(function (input) {
         input.addEventListener('input', function () {
-            updateTheme(input);
+            updateTheme(input.name, input.value);
         });
 
         // Update the theme for the first time so the pre area is filled with the values
-        updateTheme(input);
+        updateTheme(input.name, input.value);
     });
-}
-
-/**
- * Update the theme with the user's changes.
- *
- * @param el
- */
-function updateTheme(el) {
-    var name = el.name,
-        value = el.value;
-
-    if (name == 'item_selected_color' || name == 'action_list_selected_color') {
-        value = lightenDarkenColor(value, 100);
-    }
-
-    // Update the style in realtime
-    document.documentElement.style.setProperty('--' + name, value, "");
-
-    // Update the to be qss file
-    var els = document.querySelectorAll('.' + name);
-    for (var i = 0; i < els.length; i++) {
-        value = value.replace(/#/g, '');
-        els[i].innerHTML = value;
-    }
 }
 
 /**
@@ -63,26 +46,66 @@ function initClipboard() {
     });
 }
 
+function loadThemes(callback) {
+    var XmlHttpRequest = new XMLHttpRequest();
+    XmlHttpRequest.overrideMimeType("application/json");
+    XmlHttpRequest.open('GET', '../themes.json', true);
+    XmlHttpRequest.onreadystatechange = function () {
+        if (XmlHttpRequest.readyState == 4 && XmlHttpRequest.status == "200") {
+            themes = JSON.parse(XmlHttpRequest.responseText);
+
+            // We set the first theme to be the base theme, since it's the default one in albert
+            for (var prop in themes[defaultTheme]) {
+                updateTheme(prop, themes[defaultTheme][prop]);
+
+                var els = document.querySelectorAll("[name='" + prop + "']");
+                for (var i = 0; i < els.length; i++) {
+                    els[i].value = themes[defaultTheme][prop];
+                }
+            }
+
+            callback();
+        }
+    }
+    XmlHttpRequest.send(null);
+}
+
 /**
- * https://css-tricks.com/snippets/javascript/lighten-darken-color/
- * @returns {string}
+ * Update the theme with the user's changes.
+ *
+ * @param el
  */
-function lightenDarkenColor(col, amt) {
-    var usePound = false;
-    if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
+function updateTheme(propName, propValue) {
+    var original = propValue;
+
+    if (propName == 'item_selected_color' || propName == 'action_list_selected_color') {
+        propValue = hexToRgbA(propValue, 0.2);
     }
 
-    var num = parseInt(col, 16);
-    var r = (num >> 16) + amt;
-    if (r > 255) r = 255;
-    else if (r < 0) r = 0;
-    var b = ((num >> 8) & 0x00FF) + amt;
-    if (b > 255) b = 255;
-    else if (b < 0) b = 0;
-    var g = (num & 0x0000FF) + amt;
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
-    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+    // Update the style in realtime
+    document.documentElement.style.setProperty('--' + propName, propValue, "");
+
+    // Update the to be qss file
+    var els = document.querySelectorAll('.' + propName);
+    for (var i = 0; i < els.length; i++) {
+        original = original.replace(/#/g, '');
+        els[i].innerHTML = original;
+    }
+}
+
+/**
+ *
+ * @returns {string}
+ */
+function hexToRgbA(hex, transparency) {
+    var c;
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+        c = hex.substring(1).split('');
+        if (c.length == 3) {
+            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c = '0x' + c.join('');
+        return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ', ' + transparency + ')';
+    }
+    throw new Error('Bad Hex');
 }
