7f3c01933d752a2ec0274361d5718e290ed20d80
Manuel Schneider <manuelschneid3r@gmail.com>
2017-08-11 13:25:59 +0200

    Automated Travis deployment

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+set -o errexit
+cd ./site
+git init
+git config user.name "Travis CI"
+git config user.email "deploy@travis-ci.org"
+git add .
+git commit -m "Automated Travis CI deployment"
+git push --force "https://${GITHUB_TOKEN}@github.com/albertlauncher/albertlauncher.github.io.git" master &> /dev/null
