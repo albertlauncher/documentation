2e23287bef6928eae2455f1fc7c74eb56f82ea4b
Manuel Schneider <manuelschneid3r@gmail.com>
2018-12-19 20:25:34 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git config user.name "Travis CI"
 git config user.email "deploy@travis-ci.org"
 git add .
 git commit -m "Automated Travis CI deployment"
-git push --force "https://${GITHUB_TOKEN}@github.com/albertlauncher/albertlauncher.github.io.git" master &> /dev/null
+git push --force "https://${GITHUB_TOKEN}@github.com/albertlauncher/albertlauncher.github.io.git" master # &> /dev/null
